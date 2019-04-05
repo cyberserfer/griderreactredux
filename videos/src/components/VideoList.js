afb1e2b1727212c2eada32d99c67c032b4bf6d2a
Paul Buss <cyberserfer@gmail.com>
2019-04-05 13:39:11 -0500

    finished ch 11

--- videos/src/components/VideoList.js
+++ videos/src/components/VideoList.js
@@ -5,7 +5,6 @@ const VideoList = ({ videos, onVideoSelect }) => {
     const renderedList = videos.map((video) => {
         return <VideoItem key={video.id.videoId} onVideoSelect={onVideoSelect} video={video} />;
     })
-
     return <div className="ui relaxed divided list">{renderedList}</div>
 };
 
