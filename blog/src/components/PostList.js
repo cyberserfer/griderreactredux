b7de8d05aabf010466c384e035c231be82c4eb65
Paul Buss <cyberserfer@gmail.com>
2019-10-31 15:15:50 -0500

    couple more segments done

--- blog/src/components/PostList.js
+++ blog/src/components/PostList.js
@@ -1,15 +1,20 @@
-import React, {Component} from 'react';
-import {connect}from 'react-redux';
-import {fetchPosts} from '../actions';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchPosts } from '../actions';
 
 class PostList extends Component {
     componentDidMount() {
         this.props.fetchPosts();
     }
-    
+
     render() {
+        console.log(this.props.posts);
         return <div>Post List</div>
     }
 }
 
-export default connect(null, {fetchPosts})(PostList);
+const mapStateToProps = (state) => {
+    return { posts: state.posts };
+}
+
+export default connect(mapStateToProps, { fetchPosts })(PostList);
